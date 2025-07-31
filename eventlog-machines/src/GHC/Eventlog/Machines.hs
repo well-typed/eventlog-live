1facb8d4695cd180b868a1c54e92418d0d0aacd8
Wen Kokke <wenkokke@users.noreply.github.com>
2025-07-31 18:04:46 +0100

    feat(eventlog-live-influxdb): support threading

--- eventlog-machines/src/GHC/Eventlog/Machines.hs
+++ eventlog-machines/src/GHC/Eventlog/Machines.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE DeriveFunctor #-}
-{-# LANGUAGE DeriveFoldable #-}
 {-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module GHC.Eventlog.Machines (
   -- * Machines
@@ -20,7 +22,12 @@ module GHC.Eventlog.Machines (
   dropTick,
 
   -- ** Event Analysis
+  analysis,
   analyse,
+  AnalysisState,
+  threadAnalysis,
+  analyseThread,
+  ThreadAnalysisState,
 
   -- ** Event Order
   reorderEvents,
@@ -34,35 +41,35 @@ module GHC.Eventlog.Machines (
   DecodeError (..),
 ) where
 
+import Control.Applicative (Alternative (..))
 import Control.Exception (Exception, catch, throwIO)
 import Control.Monad (when)
 import Control.Monad.IO.Class (MonadIO (..))
 import Control.Monad.Trans.Class (lift)
 import qualified Data.ByteString as BS
-import Data.Foldable (traverse_, for_)
+import Data.Coerce (coerce)
+import Data.Foldable (for_, traverse_)
 import Data.Function (fix)
 import Data.Int (Int64)
+import Data.IntMap.Strict (IntMap)
 import qualified Data.IntMap.Strict as IM
 import Data.List (partition, sortBy)
-import Data.Machine (Is, MachineT, PlanT, Process, ProcessT, await, construct, yield, repeatedly)
+import Data.Machine (Is, MachineT, PlanT, Process, ProcessT, await, construct, repeatedly, yield)
 import Data.Machine.Moore (Moore (..))
+import Data.Maybe (fromMaybe, isNothing)
 import Data.Ord (comparing)
 import Data.Text (Text)
 import Data.Word (Word64)
 import GHC.Clock (getMonotonicTimeNSec)
-import GHC.RTS.Events (
-  Event (evSpec),
-  EventInfo (UserMarker),
-  Timestamp,
-  evTime,
- )
+import GHC.RTS.Events (Event (..), EventInfo (..), Timestamp, evTime)
 import qualified GHC.RTS.Events.Analysis as EA
+import GHC.RTS.Events.Analysis.Thread (ThreadState (..), threadMachine)
 import GHC.RTS.Events.Incremental (Decoder (..), decodeEventLog)
 import qualified System.Clock as Clock
 import System.IO (Handle, hWaitForInput)
 import System.IO.Error (isEOFError)
-import Data.Maybe (fromMaybe)
-import Control.Applicative (Alternative((<|>)))
+import GHC.Stack (HasCallStack)
+import GHC.RTS.Events.Analysis (Machine(..))
 
 -------------------------------------------------------------------------------
 -- Source from handle
@@ -180,8 +187,9 @@ batchByTick = construct start
 
 dropTick :: Process (Tick a) a
 dropTick = repeatedly go
-  where
-    go = await >>= \case
+ where
+  go =
+    await >>= \case
       Item a -> yield a
       Tick -> pure ()
 
@@ -225,33 +233,128 @@ instance Exception DecodeError
 -- Running ghc-events Analysis Machines
 -------------------------------------------------------------------------------
 
-analyse ::
+newtype AnalysisState s = AnalysisState {unAnalysisState :: IntMap s}
+  deriving newtype (Semigroup, Monoid)
+
+notMember :: Int -> AnalysisState s -> Bool
+notMember k = (k `IM.notMember`) . unAnalysisState
+
+analysis ::
   (MonadIO m) =>
   (i -> Int) ->
   EA.Machine s i ->
   ProcessT m i (s, i)
-analyse key m = construct (go IM.empty)
+analysis key m = construct (go mempty)
  where
-  -- go :: (MonadIO m) => IntMap s -> PlanT (Is i) (s, i) m ()
-  go st = await >>= \case
-      i | EA.alpha m i -> do
-          let handle ms = do
-                -- The input state (initial if missing).
-                let s   = fromMaybe (EA.initial m) ms
-                -- The output state (Nothing if step fails).
-                let ms' = rightToMaybe (EA.step m s i)
-                -- The altered state in the map (deleted if final).
-                let ms'_in_st'
-                      | any (EA.final m) ms' = Nothing
-                      | otherwise = ms' <|> Just s
-                (ms', ms'_in_st')
-          let (ms', st') = IM.alterF handle (key i) st
-          for_ ms' $ \s' -> yield (s', i)
-          go st'
-        | otherwise -> go st
+  go st = do
+    i <- await
+    let (ms', st') = analyse key m st i
+    for_ ms' $ \s' -> yield (s', i)
+    go st'
+
+analyse ::
+  (i -> Int) ->
+  EA.Machine s i ->
+  AnalysisState s ->
+  i ->
+  (Maybe s, AnalysisState s)
+analyse key m st i
+  | not (EA.alpha m i) = (Nothing, st)
+  | otherwise = coerce (IM.alterF handle (key i) (coerce st))
+ where
+  handle ms = do
+    -- The input state (initial if missing).
+    let s = fromMaybe (EA.initial m) ms
+    -- The output state (Nothing if step fails).
+    let ms' = rightToMaybe (EA.step m s i)
+    (ms', ms' <|> Just s)
 
 rightToMaybe :: Either e a -> Maybe a
 rightToMaybe = either (const Nothing) Just
+
+-------------------------------------------------------------------------------
+-- Thread Analysis
+
+threadAnalysis ::
+  (MonadIO m) =>
+  ProcessT m EventInfo (ThreadState, EventInfo)
+threadAnalysis = construct (go mempty)
+ where
+  go st = do
+    i <- await
+    let (ms', st') = analyseThread st i
+    for_ ms' $ \s' -> yield (s', i)
+    go st'
+
+data ThreadAnalysisState = ThreadAnalysisState
+  { analysisState :: AnalysisState ThreadState
+  , mainThreadId :: Maybe Int
+  }
+
+instance Semigroup ThreadAnalysisState where
+  (<>) :: ThreadAnalysisState -> ThreadAnalysisState -> ThreadAnalysisState
+  tas1 <> tas2 =
+    ThreadAnalysisState
+      { analysisState = analysisState tas1 <> analysisState tas2
+      , mainThreadId = mainThreadId tas1 <|> mainThreadId tas2
+      }
+
+instance Monoid ThreadAnalysisState where
+  mempty :: ThreadAnalysisState
+  mempty = ThreadAnalysisState mempty empty
+
+{- |
+The `threadMachine` exported by "GHC.RTS.Events.Analysis.Thread" has several flaws:
+
+* GHC does not issue a `CreateThread` event for the main thread.
+* GHC may restart a thread and issue a `WakeupThread` after stopping a thread with `ThreadFinished`.
+
+This machine fixes both of those issues by accepting two new transitions:
+
+* @(ThreadInitial, RunThread) -> ThreadRunning@
+* @(ThreadFinal, WakeupThread) -> ThreadRunning@
+
+Noted the first of the two transitions is more general than _just_ accepting this transition once for the main thread.
+However, doing so greatly simplifies the code and improves its robustness.
+
+TODO: Upstream to @ghc-events@.
+-}
+threadMachine' :: EA.Machine ThreadState EventInfo
+threadMachine' = threadMachine{delta = delta'}
+ where
+  delta' ThreadInitial RunThread {} = Just ThreadRunning
+  delta' ThreadInitial WakeupThread {} = Just ThreadRunning
+  delta' s i = EA.delta threadMachine s i
+
+analyseThread ::
+  ThreadAnalysisState ->
+  EventInfo ->
+  (Maybe ThreadState, ThreadAnalysisState)
+--analyseThread st@ThreadAnalysisState{..} evSpec@RunThread {}
+--  -- NOTE: There is no `CreateThread` event for the main thread. However, the
+--  --       `threadMachine` does not (and cannot) take this into account.
+--  --       To work around the fact that there is no event announcing the main
+--  --       thread, we take the first `RunThread` with an unknown `ThreadId`
+--  --       to be the main thread.
+--  | isNothing mainThreadId && threadKey evSpec `notMember` analysisState = do
+--    let mainThreadId' = threadKey evSpec
+--    let s' = ThreadRunning
+--    let analysisState' = AnalysisState (IM.insert mainThreadId' s' (coerce analysisState))
+--    (Just s', st{mainThreadId = Just mainThreadId', analysisState = analysisState'})
+analyseThread st evSpec = do
+    let (ms', analysisState') = analyse threadKey threadMachine' (analysisState st) evSpec
+    (ms', st{analysisState = analysisState'})
+
+{- |
+Internal helper. Get the thread ID as an `Int`.
+
+__Warning__:
+This function is partial and fails if the event does not have the field
+named `thread`. The assumtion is that if @`EA.alpha` `threadMachine` evSpec@
+holds, then @`thread` evSpec@ is defined.
+-}
+threadKey :: HasCallStack => EventInfo -> Int
+threadKey = fromIntegral . thread
 
 -------------------------------------------------------------------------------
 -- Reordering buffer.
