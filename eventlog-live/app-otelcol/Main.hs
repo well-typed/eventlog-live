a8113cd305b6184afd5c556f6d7dd0019642aee0
Wen Kokke <wenkokke@users.noreply.github.com>
2025-08-05 18:51:09 +0100

    feat(eventlog-live-otelcol): add options parser

--- eventlog-live/app-otelcol/Main.hs
+++ eventlog-live/app-otelcol/Main.hs
@@ -1,4 +1,44 @@
 module Main (main) where
 
+import Options.Applicative qualified as O
+import GHC.RTS.Events (HeapProfBreakdown (..))
+import GHC.Eventlog.Live
+import GHC.Eventlog.Live.Machines (batchByTick)
+import GHC.Eventlog.Live.Options
+
 main :: IO ()
-main = putStrLn "Hello, Wen!"
+main = do
+  Options {..} <- O.execParser options
+  putStrLn "Hello, Wen!"
+
+--------------------------------------------------------------------------------
+-- Options
+--------------------------------------------------------------------------------
+
+options :: O.ParserInfo Options
+options = O.info (optionsParser O.<**> O.helper) O.idm
+
+data Options = Options
+  { eventlogSocket :: EventlogSocket
+  , batchInterval :: Int
+  , maybeEventlogLogFile :: Maybe FilePath
+  , maybeHeapProfBreakdown :: Maybe HeapProfBreakdown
+  , openTelemetryCollectorOptions :: OpenTelemetryCollectorOptions
+  }
+
+optionsParser :: O.Parser Options
+optionsParser =
+  Options
+    <$> eventlogSocketParser
+    <*> batchIntervalParser
+    <*> O.optional eventlogLogFileParser
+    <*> O.optional heapProfBreakdownParser
+    <*> openTelemetryCollectorOptionsParser
+
+--------------------------------------------------------------------------------
+-- OpenTelemetry Collector Configuration
+
+data OpenTelemetryCollectorOptions = OpenTelemetryCollectorOptions
+
+openTelemetryCollectorOptionsParser :: O.Parser OpenTelemetryCollectorOptions
+openTelemetryCollectorOptionsParser = pure OpenTelemetryCollectorOptions
