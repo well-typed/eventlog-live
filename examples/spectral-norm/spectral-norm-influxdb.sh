ec836f136a3a075808d351d11fe0f9ef178573ac
Wen Kokke <wenkokke@users.noreply.github.com>
2025-08-05 17:57:22 +0100

    fix(examples): use eventlog-live-influxdb

--- examples/spectral-norm/spectral-norm-influxdb.sh
+++ examples/spectral-norm/spectral-norm-influxdb.sh
@@ -7,9 +7,9 @@ export GHC_EVENTLOG_SOCKET="/tmp/spectral_norm_eventlog.sock"
 echo "Build spectral-norm"
 cabal build spectral-norm -v0
 
-# Build eventlog-influxdb
-echo "Build eventlog-influxdb"
-cabal build eventlog-influxdb -v0
+# Build eventlog-live-influxdb
+echo "Build eventlog-live-influxdb"
+cabal build eventlog-live-influxdb -v0
 
 # Install cleanup handler
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
@@ -20,11 +20,11 @@ SPECTRAL_NORM_BIN=$(cabal list-bin exe:spectral-norm -v0 | head -n1)
 "${SPECTRAL_NORM_BIN}" 15000 20000 25000 30000 15000 20000 25000 30000 15000 20000 25000 30000 +RTS -N -l -hT --eventlog-flush-interval=1 -RTS >/dev/null &
 SPECTRAL_NORM_PID=$!
 
-# Run eventlog-influxdb
+# Run eventlog-live-influxdb
 # NOTE: The purpose of 'sleep 5' is to give the spectral-norm process
 #       sufficient time to create the Unix socket.
-echo "Start eventlog-influxdb"
-sleep 5 && cabal run eventlog-influxdb -v0 -- \
+echo "Start eventlog-live-influxdb"
+sleep 5 && cabal run eventlog-live-influxdb -v0 -- \
     --eventlog-socket "$GHC_EVENTLOG_SOCKET" \
     -hT \
     --influxdb-host=localhost \
