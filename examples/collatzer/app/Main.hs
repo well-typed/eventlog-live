8a885c076e01ad421ae3b53fec8930dd17ae5714
Wen Kokke <wenkokke@users.noreply.github.com>
2025-08-12 18:22:03 +0100

    chore: reformat code to use 2 spaces

--- examples/collatzer/app/Main.hs
+++ examples/collatzer/app/Main.hs
@@ -9,11 +9,11 @@ import Data.Foldable (for_)
 import Data.Word (Word64)
 import System.Console.Concurrent (outputConcurrent)
 import System.Console.Regions (
-    RegionLayout (Linear),
-    displayConsoleRegions,
-    finishConsoleRegion,
-    setConsoleRegion,
-    withConsoleRegion,
+  RegionLayout (Linear),
+  displayConsoleRegions,
+  finishConsoleRegion,
+  setConsoleRegion,
+  withConsoleRegion,
  )
 import System.IO (Handle)
 import Text.Printf (PrintfArg, PrintfType, printf)
@@ -38,43 +38,43 @@ import Data.Machine.Fanout (fanout)
 
 main :: IO ()
 main = displayConsoleRegions $ do
-    opts <-
-        O.execParser $
-            O.info (optsP <**> O.helper) $
-                O.fullDesc
-                    <> O.header "collatzer - demo web app"
+  opts <-
+    O.execParser $
+      O.info (optsP <**> O.helper) $
+        O.fullDesc
+          <> O.header "collatzer - demo web app"
 
-    hdl <- case optPathType opts of
-        PathTypeSocket -> connectToEventlogSocket (optEventLogPath opts)
-        PathTypeFile -> openEventlogFile (optEventLogPath opts)
+  hdl <- case optPathType opts of
+    PathTypeSocket -> connectToEventlogSocket (optEventLogPath opts)
+    PathTypeFile -> openEventlogFile (optEventLogPath opts)
 
-    let input :: MachineT IO k (Tick BS.ByteString)
-        input = sourceHandleWait 1_000_000 4096 hdl
+  let input :: MachineT IO k (Tick BS.ByteString)
+      input = sourceHandleWait 1_000_000 4096 hdl
 
-    let events :: ProcessT IO (Tick BS.ByteString) Event
-        events =
-            decodeEventBatch
-                ~> sortEventsUpTo 1_000_000_000
-                ~> handleOutOfOrderEvents (curry print)
+  let events :: ProcessT IO (Tick BS.ByteString) Event
+      events =
+        decodeEventBatch
+          ~> sortEventsUpTo 1_000_000_000
+          ~> handleOutOfOrderEvents (curry print)
 
-    counters <- atomically newCounters
+  counters <- atomically newCounters
 
-    for_ (optEkgPort opts) $ \port -> do
-        ekg <- EKG.newStore
-        registerCounters counters ekg
-        System.Remote.Monitoring.forkServerWith ekg "localhost" port
+  for_ (optEkgPort opts) $ \port -> do
+    ekg <- EKG.newStore
+    registerCounters counters ekg
+    System.Remote.Monitoring.forkServerWith ekg "localhost" port
 
-    let count' :: ProcessT IO Event a
-        count' = repeatedly $ do
-            ev <- await
-            liftIO $ atomically $ count counters ev
+  let count' :: ProcessT IO Event a
+      count' = repeatedly $ do
+        ev <- await
+        liftIO $ atomically $ count counters ev
 
-    withDisplay counters $ do
-        putStrLn "Starting..."
-        case optToFile opts of
-            Nothing -> runT_ $ input ~> events ~> count'
-            Just fp -> IO.withFile fp IO.WriteMode $ \whdl ->
-                runT_ $ input ~> fanout [events ~> count', fileSinkBatch whdl]
+  withDisplay counters $ do
+    putStrLn "Starting..."
+    case optToFile opts of
+      Nothing -> runT_ $ input ~> events ~> count'
+      Just fp -> IO.withFile fp IO.WriteMode $ \whdl ->
+        runT_ $ input ~> fanout [events ~> count', fileSinkBatch whdl]
 
 -------------------------------------------------------------------------------
 -- connecting & opening
@@ -82,30 +82,30 @@ main = displayConsoleRegions $ do
 
 connectToEventlogSocket :: FilePath -> IO Handle
 connectToEventlogSocket socketName = do
-    s <- Sock.socket Sock.AF_UNIX Sock.Stream Sock.defaultProtocol
-    Sock.connect s (Sock.SockAddrUnix socketName)
-    h <- Sock.socketToHandle s IO.ReadMode
-    IO.hSetBuffering h IO.NoBuffering
-    return h
+  s <- Sock.socket Sock.AF_UNIX Sock.Stream Sock.defaultProtocol
+  Sock.connect s (Sock.SockAddrUnix socketName)
+  h <- Sock.socketToHandle s IO.ReadMode
+  IO.hSetBuffering h IO.NoBuffering
+  return h
 
 openEventlogFile :: FilePath -> IO Handle
 openEventlogFile path = do
-    hdl <- IO.openFile path IO.ReadMode
-    IO.hSetBinaryMode hdl True
-    return hdl
+  hdl <- IO.openFile path IO.ReadMode
+  IO.hSetBinaryMode hdl True
+  return hdl
 
 -------------------------------------------------------------------------------
 -- output-concurrent + printf
 -------------------------------------------------------------------------------
 
 class PrintfConcurrent t where
-    printfConcurrent' :: (forall r. (PrintfType r) => r) -> t
+  printfConcurrent' :: (forall r. (PrintfType r) => r) -> t
 
 instance (a ~ ()) => PrintfConcurrent (IO a) where
-    printfConcurrent' s = outputConcurrent (s :: String)
+  printfConcurrent' s = outputConcurrent (s :: String)
 
 instance (PrintfArg a, PrintfConcurrent r) => PrintfConcurrent (a -> r) where
-    printfConcurrent' f a = printfConcurrent (f a)
+  printfConcurrent' f a = printfConcurrent (f a)
 
 printfConcurrent :: (PrintfConcurrent t) => String -> t
 printfConcurrent s = printfConcurrent' (printf s)
@@ -116,119 +116,119 @@ printfConcurrent s = printfConcurrent' (printf s)
 
 withDisplay :: Counters -> IO a -> IO a
 withDisplay Counters{..} action0 =
-    with textTime $
-        with textCaps $
-            with textGc $
-                with textThread $
-                    with
-                        textHeap
-                        action0
-  where
-    textTime = do
-        t <- readTVar cntTime
-        n <- readTVar cntEvents
-        return $ printf "Time: %s; Events: %s" (show t) (show n)
+  with textTime $
+    with textCaps $
+      with textGc $
+        with textThread $
+          with
+            textHeap
+            action0
+ where
+  textTime = do
+    t <- readTVar cntTime
+    n <- readTVar cntEvents
+    return $ printf "Time: %s; Events: %s" (show t) (show n)
 
-    textCaps = do
-        c <- readTVar cntCaps
-        return $ printf "Seen capabilities: %s" (show (IS.toList c))
+  textCaps = do
+    c <- readTVar cntCaps
+    return $ printf "Seen capabilities: %s" (show (IS.toList c))
 
-    textGc = do
-        maxGcTime <- readTVar cntMaxGCTime
-        gcCount0 <- readTVar cntGcCount0
-        gcCountS <- readTVar cntGcCountS
-        return $ printf "Max GC time: %.06f sec; GCs seen: %d + %d" (fromIntegral maxGcTime / 1e9 :: Double) gcCount0 gcCountS
+  textGc = do
+    maxGcTime <- readTVar cntMaxGCTime
+    gcCount0 <- readTVar cntGcCount0
+    gcCountS <- readTVar cntGcCountS
+    return $ printf "Max GC time: %.06f sec; GCs seen: %d + %d" (fromIntegral maxGcTime / 1e9 :: Double) gcCount0 gcCountS
 
-    textThread = do
-        threads <- readTVar cntThreads
+  textThread = do
+    threads <- readTVar cntThreads
 
-        let countThreads :: Int -> Int -> Int -> Int -> [ThreadState] -> (Int, Int, Int, Int)
-            countThreads !n m p r [] = (n, m, p, r)
-            countThreads !n m p r (ThreadRunning : xs) = countThreads (n + 1) m p r xs
-            countThreads !n m p r (ThreadQueued : xs) = countThreads n (m + 1) p r xs
-            countThreads !n m p r (ThreadStopped : xs) = countThreads n m (p + 1) r xs
-            countThreads !n m p r (ThreadUnknown : xs) = countThreads n m p (r + 1) xs
+    let countThreads :: Int -> Int -> Int -> Int -> [ThreadState] -> (Int, Int, Int, Int)
+        countThreads !n m p r [] = (n, m, p, r)
+        countThreads !n m p r (ThreadRunning : xs) = countThreads (n + 1) m p r xs
+        countThreads !n m p r (ThreadQueued : xs) = countThreads n (m + 1) p r xs
+        countThreads !n m p r (ThreadStopped : xs) = countThreads n m (p + 1) r xs
+        countThreads !n m p r (ThreadUnknown : xs) = countThreads n m p (r + 1) xs
 
-        let (running, queued, stopped, unknown) = countThreads 0 0 0 0 $ Map.elems threads
+    let (running, queued, stopped, unknown) = countThreads 0 0 0 0 $ Map.elems threads
 
-        return $ printf "Running threads: %d; queued: %d; stopped: %d; unknown state: %d" running queued stopped unknown
-    textHeap = do
-        s <- readTVar cntHeapSize
-        l <- readTVar cntHeapLive
-        a <- readTVar cntHeapAlloc
+    return $ printf "Running threads: %d; queued: %d; stopped: %d; unknown state: %d" running queued stopped unknown
+  textHeap = do
+    s <- readTVar cntHeapSize
+    l <- readTVar cntHeapLive
+    a <- readTVar cntHeapAlloc
 
-        return $ printf "Heap: size %s; live %s; alloc %s" (humaniseW64 s) (humaniseW64 l) (humaniseW64 a)
+    return $ printf "Heap: size %s; live %s; alloc %s" (humaniseW64 s) (humaniseW64 l) (humaniseW64 a)
 
-    with :: STM String -> IO a -> IO a
-    with regionText action = do
-        withConsoleRegion Linear $ \reg -> do
-            setConsoleRegion reg (fmap T.pack regionText)
-            res <- action
-            text <- atomically regionText
-            finishConsoleRegion reg text
-            return res
+  with :: STM String -> IO a -> IO a
+  with regionText action = do
+    withConsoleRegion Linear $ \reg -> do
+      setConsoleRegion reg (fmap T.pack regionText)
+      res <- action
+      text <- atomically regionText
+      finishConsoleRegion reg text
+      return res
 
-    humaniseW64 :: Word64 -> String
-    humaniseW64 = humanise . fromIntegral
+  humaniseW64 :: Word64 -> String
+  humaniseW64 = humanise . fromIntegral
 
-    humanise :: Double -> String
-    humanise = go 0
-      where
-        go :: Int -> Double -> String
-        go k d
-            | d < 10 = printf "%.03f %s" d (unit k)
-            | d < 100 = printf "%.02f %s" d (unit k)
-            | d < 1000 = printf "%.01f %s" d (unit k)
-            | otherwise = go (k + 1) (d / 1000)
+  humanise :: Double -> String
+  humanise = go 0
+   where
+    go :: Int -> Double -> String
+    go k d
+      | d < 10 = printf "%.03f %s" d (unit k)
+      | d < 100 = printf "%.02f %s" d (unit k)
+      | d < 1000 = printf "%.01f %s" d (unit k)
+      | otherwise = go (k + 1) (d / 1000)
 
-        unit :: Int -> String
-        unit 0 = ""
-        unit 1 = "k"
-        unit 2 = "M"
-        unit 3 = "G"
-        unit 4 = "T"
-        unit 5 = "E"
-        unit _ = "alot"
+    unit :: Int -> String
+    unit 0 = ""
+    unit 1 = "k"
+    unit 2 = "M"
+    unit 3 = "G"
+    unit 4 = "T"
+    unit 5 = "E"
+    unit _ = "alot"
 
 -------------------------------------------------------------------------------
 -- Options
 -------------------------------------------------------------------------------
 
 data PathType = PathTypeSocket | PathTypeFile
-    deriving (Show)
+  deriving (Show)
 
 data Opts = Opts
-    { optEventLogPath :: FilePath
-    , optPathType :: PathType
-    , optEkgPort :: Maybe Int
-    , optToFile :: Maybe FilePath
-    }
-    deriving (Show)
+  { optEventLogPath :: FilePath
+  , optPathType :: PathType
+  , optEkgPort :: Maybe Int
+  , optToFile :: Maybe FilePath
+  }
+  deriving (Show)
 
 optsP :: O.Parser Opts
 optsP = do
-    optEventLogPath <-
-        O.strArgument $
-            O.metavar "PATH"
-                <> O.help "Eventlog path"
-    optPathType <- pathTypeP
-    optEkgPort <-
-        optional $
-            O.option O.auto $
-                O.long "ekg"
-                    <> O.metavar "PORT"
-                    <> O.help "EKG server port"
-    optToFile <-
-        optional $
-            O.strOption $
-                O.long "write-to-file"
-                    <> O.metavar "PATH"
-                    <> O.help "Additionally write received eventlog to file"
+  optEventLogPath <-
+    O.strArgument $
+      O.metavar "PATH"
+        <> O.help "Eventlog path"
+  optPathType <- pathTypeP
+  optEkgPort <-
+    optional $
+      O.option O.auto $
+        O.long "ekg"
+          <> O.metavar "PORT"
+          <> O.help "EKG server port"
+  optToFile <-
+    optional $
+      O.strOption $
+        O.long "write-to-file"
+          <> O.metavar "PATH"
+          <> O.help "Additionally write received eventlog to file"
 
-    pure Opts{..}
-  where
-    pathTypeP :: O.Parser PathType
-    pathTypeP =
-        O.flag' PathTypeSocket (O.long "unix" <> O.help "Path is to UNIX socket (default)")
-            <|> O.flag' PathTypeFile (O.long "file" <> O.help "Path is to ordinary file")
-            <|> pure PathTypeSocket
+  pure Opts{..}
+ where
+  pathTypeP :: O.Parser PathType
+  pathTypeP =
+    O.flag' PathTypeSocket (O.long "unix" <> O.help "Path is to UNIX socket (default)")
+      <|> O.flag' PathTypeFile (O.long "file" <> O.help "Path is to ordinary file")
+      <|> pure PathTypeSocket
--- examples/collatzer/src-counters/GHC/Eventlog/Counters.hs
+++ examples/collatzer/src-counters/GHC/Eventlog/Counters.hs
@@ -1,12 +1,12 @@
 module GHC.Eventlog.Counters where
 
 import Control.Concurrent.STM (
-    STM,
-    TVar,
-    modifyTVar',
-    newTVar,
-    readTVar,
-    writeTVar,
+  STM,
+  TVar,
+  modifyTVar',
+  newTVar,
+  readTVar,
+  writeTVar,
  )
 import Data.Foldable (for_)
 import Data.IntSet (IntSet)
@@ -20,130 +20,130 @@ import GHC.RTS.Events
 
 -- | Various counters we update with 'count'.
 data Counters = Counters
-    { cntEvents :: TVar Word64
-    , cntTime :: TVar Timestamp
-    , cntWallClock :: TVar (Maybe WallClockInfo)
-    , cntCaps :: TVar IntSet
-    , cntHeapSize :: TVar Word64
-    , cntHeapLive :: TVar Word64
-    , cntBlocksSize :: TVar Word64
-    , cntHeapAlloc :: TVar Word64
-    , cntBlocksCurrent :: TVar Word32
-    , cntBlocksNeeded :: TVar Word32
-    , cntBlocksReturned :: TVar Word32
-    , cntGCStart :: TVar (Maybe Timestamp)
-    , cntMaxGCTime :: TVar Timestamp
-    , cntGcCount0 :: TVar Word64
-    , cntGcCountS :: TVar Word64
-    , cntThreads :: TVar (Map ThreadId ThreadState)
-    }
+  { cntEvents :: TVar Word64
+  , cntTime :: TVar Timestamp
+  , cntWallClock :: TVar (Maybe WallClockInfo)
+  , cntCaps :: TVar IntSet
+  , cntHeapSize :: TVar Word64
+  , cntHeapLive :: TVar Word64
+  , cntBlocksSize :: TVar Word64
+  , cntHeapAlloc :: TVar Word64
+  , cntBlocksCurrent :: TVar Word32
+  , cntBlocksNeeded :: TVar Word32
+  , cntBlocksReturned :: TVar Word32
+  , cntGCStart :: TVar (Maybe Timestamp)
+  , cntMaxGCTime :: TVar Timestamp
+  , cntGcCount0 :: TVar Word64
+  , cntGcCountS :: TVar Word64
+  , cntThreads :: TVar (Map ThreadId ThreadState)
+  }
 
 {- | Wall-clock information.
 
 RTS issues an event so we can connect event timestamps with the wall clock.
 -}
 data WallClockInfo = WallClockInfo
-    { wciTime :: !Timestamp
-    -- ^ event timestamp
-    , wciSec :: !Word64
-    -- ^ wall clock seconds
-    , wciNSec :: !Word32
-    -- ^ wall clock nanoseconds
-    }
+  { wciTime :: !Timestamp
+  -- ^ event timestamp
+  , wciSec :: !Word64
+  -- ^ wall clock seconds
+  , wciNSec :: !Word32
+  -- ^ wall clock nanoseconds
+  }
 
 -- | Thread states, used in 'cntThreads'.
 data ThreadState
-    = -- | we know thread exist, but not its state
-      ThreadUnknown
-    | -- | currently running
-      ThreadRunning
-    | -- | ready to be run
-      ThreadQueued
-    | -- | stopped, e.g. blocked
-      ThreadStopped
-    deriving (Eq, Show)
+  = -- | we know thread exist, but not its state
+    ThreadUnknown
+  | -- | currently running
+    ThreadRunning
+  | -- | ready to be run
+    ThreadQueued
+  | -- | stopped, e.g. blocked
+    ThreadStopped
+  deriving (Eq, Show)
 
 instance Semigroup ThreadState where
-    ThreadUnknown <> y = y
-    x <> ThreadUnknown = x
-    x <> _ = x
+  ThreadUnknown <> y = y
+  x <> ThreadUnknown = x
+  x <> _ = x
 
 newCounters :: STM Counters
 newCounters = do
-    cntEvents <- newTVar 0
-    cntTime <- newTVar 0
-    cntWallClock <- newTVar Nothing
-    cntCaps <- newTVar IS.empty
+  cntEvents <- newTVar 0
+  cntTime <- newTVar 0
+  cntWallClock <- newTVar Nothing
+  cntCaps <- newTVar IS.empty
 
-    cntHeapSize <- newTVar 0
-    cntHeapLive <- newTVar 0
-    cntHeapAlloc <- newTVar 0
-    cntBlocksSize <- newTVar 0
+  cntHeapSize <- newTVar 0
+  cntHeapLive <- newTVar 0
+  cntHeapAlloc <- newTVar 0
+  cntBlocksSize <- newTVar 0
 
-    cntBlocksCurrent <- newTVar 0
-    cntBlocksNeeded <- newTVar 0
-    cntBlocksReturned <- newTVar 0
+  cntBlocksCurrent <- newTVar 0
+  cntBlocksNeeded <- newTVar 0
+  cntBlocksReturned <- newTVar 0
 
-    cntGCStart <- newTVar Nothing
-    cntMaxGCTime <- newTVar 0
-    cntGcCount0 <- newTVar 0
-    cntGcCountS <- newTVar 0
+  cntGCStart <- newTVar Nothing
+  cntMaxGCTime <- newTVar 0
+  cntGcCount0 <- newTVar 0
+  cntGcCountS <- newTVar 0
 
-    cntThreads <- newTVar Map.empty
+  cntThreads <- newTVar Map.empty
 
-    return Counters{..}
+  return Counters{..}
 
 count :: Counters -> Event -> STM ()
 count cnt (Event t s c) = do
-    -- Count events
-    modifyTVar' (cntEvents cnt) (+ 1)
+  -- Count events
+  modifyTVar' (cntEvents cnt) (+ 1)
 
-    -- Update time
-    t' <- readTVar (cntTime cnt)
-    writeTVar (cntTime cnt) (max t t')
+  -- Update time
+  t' <- readTVar (cntTime cnt)
+  writeTVar (cntTime cnt) (max t t')
 
-    -- update seen capabilities
-    for_ c $ \c' -> modifyTVar' (cntCaps cnt) (IS.insert c')
+  -- update seen capabilities
+  for_ c $ \c' -> modifyTVar' (cntCaps cnt) (IS.insert c')
 
-    case s of
-        -- real time
-        WallClockTime{sec, nsec} ->
-            writeTVar (cntWallClock cnt) $
-                Just
-                    WallClockInfo
-                        { wciTime = t
-                        , wciSec = sec
-                        , wciNSec = nsec
-                        }
-        -- Thread events
-        WakeupThread{..} -> modifyTVar' (cntThreads cnt) (Map.insertWith (<>) thread ThreadQueued)
-        CreateThread{..} -> modifyTVar' (cntThreads cnt) (Map.insertWith (<>) thread ThreadQueued)
-        RunThread{..} -> modifyTVar' (cntThreads cnt) (Map.insertWith (<>) thread ThreadRunning)
-        MigrateThread{..} -> modifyTVar' (cntThreads cnt) (Map.insertWith (<>) thread ThreadUnknown)
-        ThreadLabel{..} -> modifyTVar' (cntThreads cnt) (Map.insertWith (<>) thread ThreadUnknown)
-        StopThread{..} -> case status of
-            ThreadFinished -> modifyTVar' (cntThreads cnt) (Map.delete thread)
-            ForeignCall -> modifyTVar' (cntThreads cnt) (Map.insertWith (<>) thread ThreadQueued)
-            HeapOverflow -> modifyTVar' (cntThreads cnt) (Map.insertWith (<>) thread ThreadQueued)
-            StackOverflow -> modifyTVar' (cntThreads cnt) (Map.insertWith (<>) thread ThreadQueued)
-            _ -> modifyTVar' (cntThreads cnt) (Map.insertWith (<>) thread ThreadStopped)
-        -- GC events
-        StartGC -> writeTVar (cntGCStart cnt) (Just t)
-        EndGC -> do
-            mt <- readTVar (cntGCStart cnt)
-            case mt of
-                Nothing -> return () -- shouldn't happen, but may.
-                Just start -> modifyTVar' (cntMaxGCTime cnt) (max (t - start))
+  case s of
+    -- real time
+    WallClockTime{sec, nsec} ->
+      writeTVar (cntWallClock cnt) $
+        Just
+          WallClockInfo
+            { wciTime = t
+            , wciSec = sec
+            , wciNSec = nsec
+            }
+    -- Thread events
+    WakeupThread{..} -> modifyTVar' (cntThreads cnt) (Map.insertWith (<>) thread ThreadQueued)
+    CreateThread{..} -> modifyTVar' (cntThreads cnt) (Map.insertWith (<>) thread ThreadQueued)
+    RunThread{..} -> modifyTVar' (cntThreads cnt) (Map.insertWith (<>) thread ThreadRunning)
+    MigrateThread{..} -> modifyTVar' (cntThreads cnt) (Map.insertWith (<>) thread ThreadUnknown)
+    ThreadLabel{..} -> modifyTVar' (cntThreads cnt) (Map.insertWith (<>) thread ThreadUnknown)
+    StopThread{..} -> case status of
+      ThreadFinished -> modifyTVar' (cntThreads cnt) (Map.delete thread)
+      ForeignCall -> modifyTVar' (cntThreads cnt) (Map.insertWith (<>) thread ThreadQueued)
+      HeapOverflow -> modifyTVar' (cntThreads cnt) (Map.insertWith (<>) thread ThreadQueued)
+      StackOverflow -> modifyTVar' (cntThreads cnt) (Map.insertWith (<>) thread ThreadQueued)
+      _ -> modifyTVar' (cntThreads cnt) (Map.insertWith (<>) thread ThreadStopped)
+    -- GC events
+    StartGC -> writeTVar (cntGCStart cnt) (Just t)
+    EndGC -> do
+      mt <- readTVar (cntGCStart cnt)
+      case mt of
+        Nothing -> return () -- shouldn't happen, but may.
+        Just start -> modifyTVar' (cntMaxGCTime cnt) (max (t - start))
 
-        -- stats
-        GCStatsGHC{..} -> do
-            if gen == 0 then modifyTVar' (cntGcCount0 cnt) (+ 1) else modifyTVar' (cntGcCountS cnt) (+ 1)
-        HeapAllocated{allocBytes = x} -> writeTVar (cntHeapAlloc cnt) x
-        HeapSize{sizeBytes = x} -> writeTVar (cntHeapSize cnt) x
-        BlocksSize{blocksSize = x} -> writeTVar (cntBlocksSize cnt) x
-        MemReturn{..} -> do
-            writeTVar (cntBlocksCurrent cnt) current
-            writeTVar (cntBlocksNeeded cnt) needed
-            writeTVar (cntBlocksReturned cnt) returned
-        HeapLive{liveBytes = x} -> writeTVar (cntHeapLive cnt) x
-        _otherwise -> pure ()
+    -- stats
+    GCStatsGHC{..} -> do
+      if gen == 0 then modifyTVar' (cntGcCount0 cnt) (+ 1) else modifyTVar' (cntGcCountS cnt) (+ 1)
+    HeapAllocated{allocBytes = x} -> writeTVar (cntHeapAlloc cnt) x
+    HeapSize{sizeBytes = x} -> writeTVar (cntHeapSize cnt) x
+    BlocksSize{blocksSize = x} -> writeTVar (cntBlocksSize cnt) x
+    MemReturn{..} -> do
+      writeTVar (cntBlocksCurrent cnt) current
+      writeTVar (cntBlocksNeeded cnt) needed
+      writeTVar (cntBlocksReturned cnt) returned
+    HeapLive{liveBytes = x} -> writeTVar (cntHeapLive cnt) x
+    _otherwise -> pure ()
