3cec7834e41ccea650ad64750b4494252805448a
Wen Kokke <wenkokke@users.noreply.github.com>
2025-09-01 12:19:17 +0100

    feat(eventlog-live): add verbosity parser to options sub-library

--- eventlog-live/src-options/GHC/Eventlog/Live/Options.hs
+++ eventlog-live/src-options/GHC/Eventlog/Live/Options.hs
@@ -3,12 +3,15 @@ module GHC.Eventlog.Live.Options (
   heapProfBreakdownParser,
   eventlogLogFileParser,
   batchIntervalParser,
+  verbosityParser,
 ) where
 
+import Data.Char (toLower)
 import GHC.Eventlog.Live (EventlogSocket (..))
-import GHC.Eventlog.Live.Machines (heapProfBreakdownEitherReader)
+import GHC.Eventlog.Live.Machines (Verbosity, heapProfBreakdownEitherReader, verbosityError, verbosityQuiet, verbosityWarning)
 import GHC.RTS.Events (HeapProfBreakdown (..))
 import Options.Applicative qualified as O
+import Text.Read (readEither)
 
 --------------------------------------------------------------------------------
 -- Eventlog Socket
@@ -62,3 +65,33 @@ batchIntervalParser =
 
 defaultBatchIntervalMs :: Int
 defaultBatchIntervalMs = 1_000
+
+--------------------------------------------------------------------------------
+-- Verbosity
+
+verbosityParser :: O.Parser Verbosity
+verbosityParser =
+  O.option
+    (O.eitherReader readEitherVerbosity)
+    ( O.short 'v'
+        <> O.long "verbosity"
+        <> O.metavar "[N|quiet|error|warning]"
+        <> O.help "The verbosity threshold for logging."
+        <> O.value verbosityWarning
+    )
+
+readEitherVerbosity :: String -> Either String Verbosity
+readEitherVerbosity rawVerbosity =
+  -- try to parse the verbosity as a number...
+  case readEither @Word rawVerbosity of
+    -- if the verbosity string is a number, map it to a verbosity...
+    Right verbosityThreshold
+      | verbosityThreshold <= 0 -> Right verbosityQuiet
+      | verbosityThreshold == 1 -> Right verbosityError
+      | otherwise -> Right verbosityWarning
+    -- otherwise, match it against the literal names of the levels...
+    Left _parseError -> case toLower <$> rawVerbosity of
+      "quiet" -> Right verbosityQuiet
+      "error" -> Right verbosityError
+      "warning" -> Right verbosityWarning
+      _otherwise -> Left $ "Could not parse verbosity '" <> rawVerbosity <> "'."
