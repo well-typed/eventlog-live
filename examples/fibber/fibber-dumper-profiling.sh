5da43bc93cb0d2c451295aea9a7003c9c68923b5
Wen Kokke <wenkokke@users.noreply.github.com>
2025-09-11 17:38:21 +0100

    feat(examples/fibber): add fibber-dumper-profiling

---
+++ examples/fibber/fibber-dumper-profiling.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -e
+
+# Set the eventlog socket
+export GHC_EVENTLOG_SOCKET="/tmp/fibber_eventlog.sock"
+
+# Build fibber
+echo "Build fibber"
+cabal build fibber -v0 --project-file=cabal.project.profiling
+
+# Build dumper
+echo "Build dumper"
+cabal build dumper -v0
+
+# Install cleanup handler
+trap 'trap - TERM && kill -- -$$' INT TERM EXIT
+
+# Run fibber
+echo "Start fibber"
+FIBBER_BIN=$(cabal list-bin exe:fibber -v0 --project-file=cabal.project.profiling | head -n1)
+"${FIBBER_BIN}" 44 +RTS -l -p --eventlog-flush-interval=1 -RTS >/dev/null &
+FIBBER_PID=$!
+
+# Run dumper
+# NOTE: The purpose of 'sleep 5' is to give the fibber process
+#       sufficient time to create the Unix socket.
+echo "Start dumper"
+sleep 5 && cabal run dumper -v0 -- --eventlog-socket "$GHC_EVENTLOG_SOCKET" "$@"
+
+# Wait for fibber to finish
+wait $FIBBER_PID
