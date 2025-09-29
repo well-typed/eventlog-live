3f14b4652198695087386d2597abde8b2521d18a
Wen Kokke <wenkokke@users.noreply.github.com>
2025-09-29 16:50:06 +0100

    refactor(eventlog-live): move attributes, metrics, and spans into their own modules

---
+++ eventlog-live/src-machines/GHC/Eventlog/Live/Data/Metric.hs
@@ -0,0 +1,30 @@
+{- |
+Module      : GHC.Eventlog.Live.Metric
+Description : Representation for metrics.
+Stability   : experimental
+Portability : portable
+-}
+module GHC.Eventlog.Live.Data.Metric (
+  Metric (..),
+) where
+
+import GHC.Eventlog.Live.Data.Attribute (Attr)
+import GHC.RTS.Events (Timestamp)
+
+{- |
+Metrics combine a measurement with a timestamp representing the time of the
+measurement, a timestamp representing the earliest possible measurment, and
+a list of attributes.
+-}
+data Metric a = Metric
+  { value :: !a
+  -- ^ The measurement.
+  , maybeTimeUnixNano :: !(Maybe Timestamp)
+  -- ^ The time at which the measurment was taken.
+  , maybeStartTimeUnixNano :: !(Maybe Timestamp)
+  -- ^ The earliest time at which any measurement could have been taken.
+  --   Usually, this represents the start time of a process.
+  , attr :: [Attr]
+  -- ^ A list of attributes.
+  }
+  deriving (Functor, Show)
