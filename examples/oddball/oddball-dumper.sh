5cd56d527d7852723668a6a5a8e771afa7d4c241
Wen Kokke <wenkokke@users.noreply.github.com>
2025-11-03 13:51:37 +0000

    fix(examples/oddball): fix cleanup handlers

--- examples/oddball/oddball-dumper.sh
+++ examples/oddball/oddball-dumper.sh
@@ -6,23 +6,21 @@ export GHC_EVENTLOG_SOCKET="/tmp/oddball_eventlog.sock"
 # Build oddball
 echo "Build oddball"
 cabal build oddball -v0
+ODDBALL_BIN=$(cabal list-bin exe:oddball -v0 | head -n1)
 
 # Build dumper
 echo "Build dumper"
 cabal build dumper -v0
-
-# Install cleanup handler
-trap 'trap - TERM && kill -- -$$' INT TERM
+DUMPER_BIN=$(cabal list-bin exe:dumper -v0 | head -n1)
 
 # Run oddball
 echo "Start oddball"
-ODDBALL_BIN=$(cabal list-bin exe:oddball -v0 | head -n1)
 "${ODDBALL_BIN}" +RTS -l -hT --eventlog-flush-interval=1 -RTS >/dev/null &
-ODDBALL_PID=$!
+
+# Install cleanup handler
+# shellcheck disable=SC2064
+trap "trap - TERM && kill -- -$$" INT TERM EXIT
 
 # Run dumper
 echo "Start dumper"
-cabal run dumper -v0 -- --eventlog-socket="$GHC_EVENTLOG_SOCKET" "$@"
-
-# Wait for oddball to finish
-wait $ODDBALL_PID
+"${DUMPER_BIN}" --eventlog-socket="$GHC_EVENTLOG_SOCKET" "$@"
