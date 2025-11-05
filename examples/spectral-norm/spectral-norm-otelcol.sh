9ed3f33a284d4fac46487ca07e9281a6115121e3
Wen Kokke <wenkokke@users.noreply.github.com>
2025-11-05 17:09:17 +0000

    fix(examples/spectral-norm): fix spectral-norm-otelcol

--- examples/spectral-norm/spectral-norm-otelcol.sh
+++ examples/spectral-norm/spectral-norm-otelcol.sh
@@ -6,29 +6,26 @@ export GHC_EVENTLOG_SOCKET="/tmp/spectral_norm_eventlog.sock"
 # Build spectral-norm
 echo "Build spectral-norm"
 cabal build spectral-norm -v0
+SPECTRAL_NORM_BIN=$(cabal list-bin exe:spectral-norm -v0 | head -n1)
 
 # Build eventlog-live-otelcol
 echo "Build eventlog-live-otelcol"
 cabal build eventlog-live-otelcol -v0
-
-# Install cleanup handler
-trap 'trap - TERM && kill -- -$$' INT TERM
+EVENTLOG_LIVE_OTELCOL_BIN=$(cabal list-bin exe:eventlog-live-otelcol -v0 | head -n1)
 
 # Run spectral-norm
 echo "Start spectral-norm"
-SPECTRAL_NORM_BIN=$(cabal list-bin exe:spectral-norm -v0 | head -n1)
-"${SPECTRAL_NORM_BIN}" 15000 20000 25000 30000 15000 20000 25000 30000 15000 20000 25000 30000 +RTS -N -l -hT --eventlog-flush-interval=1 -RTS >/dev/null &
-SPECTRAL_NORM_PID=$!
+"${SPECTRAL_NORM_BIN}" 15000 20000 25000 30000 15000 20000 25000 30000 15000 20000 25000 30000 +RTS -l -hT --eventlog-flush-interval=1 -RTS >/dev/null &
+
+# Install cleanup handler
+# shellcheck disable=SC2064
+trap "trap - TERM && kill -- -$$" INT TERM EXIT
 
 # Run eventlog-live-otelcol
-# NOTE: The purpose of 'sleep 5' is to give the spectral-norm process
-#       sufficient time to create the Unix socket.
 echo "Start eventlog-live-otelcol"
-cabal run eventlog-live-otelcol -v0 -- \
-    --service-name "spectral-norm" \
+"${EVENTLOG_LIVE_OTELCOL_BIN}" \
+	--verbosity=quiet \
+	--stats \
     --eventlog-socket "$GHC_EVENTLOG_SOCKET" \
     -hT \
     --otelcol-host=localhost
-
-# Wait for spectral-norm to finish
-wait $SPECTRAL_NORM_PID
