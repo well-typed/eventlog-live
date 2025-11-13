875858307ff4c6d5e312fab61e5a3ae4103eab7f
Wen Kokke <wenkokke@users.noreply.github.com>
2025-11-13 14:03:57 +0000

    refactor(examples/eventlog-live-otelcol): use screen to display all data streams

--- examples/eventlog-live-otelcol/eventlog-live-otelcol-config.sh
+++ examples/eventlog-live-otelcol/eventlog-live-otelcol-config.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/bash -
 
 # Get the script directory
 DIR=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)
@@ -17,34 +17,93 @@ echo "Build eventlog-live-otelcol"
 cabal build eventlog-live-otelcol -v0 -f+use-ghc-debug-stub
 EVENTLOG_LIVE_OTELCOL_BIN=$(cabal list-bin exe:eventlog-live-otelcol -v0 | head -n1)
 
-# Run oddball
-echo "Start oddball"
-"${ODDBALL_BIN}" +RTS -l -hT --eventlog-flush-interval=1 -RTS >/dev/null &
+# Create the temporary directory
+TMPDIR=$(mktemp -d) || exit
+trap 'rm -rf "$TMPDIR"' EXIT INT TERM HUP
 
-# Run eventlog-live-otelcol (for oddball)
-echo "Start eventlog-live-otelcol (for oddball)"
-"${EVENTLOG_LIVE_OTELCOL_BIN}" \
-	--verbosity=quiet \
+# Create the screen pipe for oddball
+ODDBALL_FIFO="$TMPDIR/oddball.fifo"
+mkfifo "$ODDBALL_FIFO" || exit
+
+# Create the screen pipe for eventlog-live-otelcol (for oddball)
+EVENTLOG_LIVE_OTELCOL_FOR_ODDBALL_FIFO=$TMPDIR/eventlog-live-otelcol-for-oddball.fifo
+mkfifo "$EVENTLOG_LIVE_OTELCOL_FOR_ODDBALL_FIFO" || exit
+
+# Create the screen pipe for eventlog-live-otelcol (for oddball)
+EVENTLOG_LIVE_OTELCOL_FOR_ITSELF_FIFO=$TMPDIR/eventlog-live-otelcol-for-itself.fifo
+mkfifo "$EVENTLOG_LIVE_OTELCOL_FOR_ITSELF_FIFO" || exit
+
+# Create the command to start oddball
+# shellcheck disable=SC2089
+ODDBALL_CMD="
+echo 'Start oddball' && \
+	${ODDBALL_BIN} \
+		+RTS \
+		-l \
+		-hT \
+		--eventlog-flush-interval=1 \
+		-RTS
+"
+
+# Create the command to start eventlog-live-otelcol (for oddball)
+# shellcheck disable=SC2089
+EVENTLOG_LIVE_OTELCOL_FOR_ODDBALL_CMD="
+echo 'Start eventlog-live-otelcol (for oddball)' && \
+	${EVENTLOG_LIVE_OTELCOL_BIN} \
+		--verbosity=debug \
+		--stats \
+		--config='$DIR/config/oddball.yaml' \
+	    --eventlog-socket '$GHC_EVENTLOG_SOCKET' \
+		--enable-my-eventlog-socket-unix '$MY_GHC_EVENTLOG_SOCKET' \
+	    -hT \
+	    --otelcol-host=localhost \
+		+RTS -l -hT --eventlog-flush-interval=1 -RTS
+"
+
+# Create the command to start eventlog-live-otelcol (for itself)
+# shellcheck disable=SC2089
+EVENTLOG_LIVE_OTELCOL_FOR_ITSELF_CMD="
+echo 'Start eventlog-live-otelcol (for itself)'
+${EVENTLOG_LIVE_OTELCOL_BIN} \
+	--verbosity=debug \
 	--stats \
-	--config="$DIR/config/oddball.yaml" \
-    --eventlog-socket "$GHC_EVENTLOG_SOCKET" \
-	--enable-my-eventlog-socket-unix "$MY_GHC_EVENTLOG_SOCKET" \
-	--enable-my-ghc-debug-socket \
+	--config='$DIR/config/eventlog-live-otelcol.yaml' \
+    --eventlog-socket '$MY_GHC_EVENTLOG_SOCKET' \
     -hT \
-    --otelcol-host=localhost \
-	+RTS -l -hT --eventlog-flush-interval=1 -RTS &
+    --otelcol-host=localhost
+"
 
-# Install cleanup handler
-# shellcheck disable=SC2064
-trap "trap - TERM && kill -- -$$" INT TERM EXIT
+# Create the screen conf file
+SCREEN_CONF="$TMPDIR/screen.conf"
+cat > "$SCREEN_CONF" << 'EOF' || exit
+split
+split
+split -v
+focus right
+screen -t 'oddball/stderr' sh -c 'tty > "$ODDBALL_FIFO"; read done < "$ODDBALL_FIFO"'
+focus left
+screen -t 'oddball/stdout' sh -c 'read tty < "$ODDBALL_FIFO"; eval "$ODDBALL_CMD" 2> "$tty"; echo "[Command exited with status $?, press enter to exit]"; read prompt; echo done > "$ODDBALL_FIFO"'
+focus down
+split -v
+focus right
+screen -t 'eventlog-live-otelcol-for-oddball/stderr' sh -c 'tty > "$EVENTLOG_LIVE_OTELCOL_FOR_ODDBALL_FIFO"; read done < "$EVENTLOG_LIVE_OTELCOL_FOR_ODDBALL_FIFO"'
+focus left
+screen -t 'eventlog-live-otelcol-for-oddball/stdout' sh -c 'read tty < "$EVENTLOG_LIVE_OTELCOL_FOR_ODDBALL_FIFO"; eval "$EVENTLOG_LIVE_OTELCOL_FOR_ODDBALL_CMD" 2> "$tty"; echo "[Command exited with status $?, press enter to exit]"; read prompt; echo done > "$EVENTLOG_LIVE_OTELCOL_FOR_ODDBALL_FIFO"'
+focus down
+split -v
+focus right
+screen -t 'eventlog-live-otelcol-for-itself/stderr' sh -c 'tty > "$EVENTLOG_LIVE_OTELCOL_FOR_ITSELF_FIFO"; read done < "$EVENTLOG_LIVE_OTELCOL_FOR_ITSELF_FIFO"'
+focus left
+screen -t 'eventlog-live-otelcol-for-itself/stdout' sh -c 'read tty < "$EVENTLOG_LIVE_OTELCOL_FOR_ITSELF_FIFO"; eval "$EVENTLOG_LIVE_OTELCOL_FOR_ITSELF_CMD" 2> "$tty"; echo "[Command exited with status $?, press enter to exit]"; read prompt; echo done > "$EVENTLOG_LIVE_OTELCOL_FOR_ITSELF_FIFO"'
+EOF
 
-# Run eventlog-live-otelcol (for itself)
-echo "Start eventlog-live-otelcol (for itself)"
-"${EVENTLOG_LIVE_OTELCOL_BIN}" \
-	--verbosity=quiet \
-	--stats \
-	--config="$DIR/config/eventlog-live-otelcol.yaml" \
-    --eventlog-socket "$MY_GHC_EVENTLOG_SOCKET" \
-    -hT \
-    --otelcol-host=localhost \
-	 >/dev/null
+# Start screen
+# shellcheck disable=SC2090
+export \
+	ODDBALL_FIFO \
+	ODDBALL_CMD \
+	EVENTLOG_LIVE_OTELCOL_FOR_ODDBALL_FIFO \
+	EVENTLOG_LIVE_OTELCOL_FOR_ODDBALL_CMD \
+	EVENTLOG_LIVE_OTELCOL_FOR_ITSELF_FIFO \
+	EVENTLOG_LIVE_OTELCOL_FOR_ITSELF_CMD
+screen -mc "$SCREEN_CONF"
