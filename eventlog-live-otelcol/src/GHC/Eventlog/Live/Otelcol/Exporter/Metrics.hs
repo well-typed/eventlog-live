7d55b49fe758aa213c044927a1819bf1c5f37cf9
Wen Kokke <wenkokke@users.noreply.github.com>
2025-11-24 13:10:07 +0000

    refactor(eventlog-live-otelcol): move exporters to separate files

--- eventlog-live-otelcol/src/GHC/Eventlog/Live/Otelcol/Exporter/Metrics.hs
+++ eventlog-live-otelcol/src/GHC/Eventlog/Live/Otelcol/Exporter/Metrics.hs
@@ -1,28 +1,10 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
 
-module GHC.Eventlog.Live.Otelcol.Exporter (
+module GHC.Eventlog.Live.Otelcol.Exporter.Metrics (
   -- * Metrics
   ExportMetricsResult (..),
   RejectedMetricsError (..),
   exportResourceMetrics,
-
-  -- * Traces
-  ExportTraceResult (..),
-  RejectedSpansError (..),
-  exportResourceSpans,
-
-  -- * Helpers
-
-  -- ** Counting Metrics
-  countDataPointsInExportMetricsServiceRequest,
-  countDataPointsInResourceMetrics,
-  countDataPointsInScopeMetrics,
-  countDataPointsInMetric,
-
-  -- ** Counting Spans
-  countSpansInExportTraceServiceRequest,
-  countSpansInResourceSpans,
-  countSpansInScopeSpans,
 ) where
 
 import Control.Exception (Exception (..), SomeException (..), catch)
@@ -42,12 +24,8 @@ import Network.GRPC.Common.Protobuf (Protobuf)
 import Network.GRPC.Common.Protobuf qualified as G
 import Proto.Opentelemetry.Proto.Collector.Metrics.V1.MetricsService qualified as OMS
 import Proto.Opentelemetry.Proto.Collector.Metrics.V1.MetricsService_Fields qualified as OMS
-import Proto.Opentelemetry.Proto.Collector.Trace.V1.TraceService qualified as OTS
-import Proto.Opentelemetry.Proto.Collector.Trace.V1.TraceService_Fields qualified as OTS
 import Proto.Opentelemetry.Proto.Metrics.V1.Metrics qualified as OM
 import Proto.Opentelemetry.Proto.Metrics.V1.Metrics_Fields qualified as OM
-import Proto.Opentelemetry.Proto.Trace.V1.Trace qualified as OT
-import Proto.Opentelemetry.Proto.Trace.V1.Trace_Fields qualified as OT
 import Text.Printf (printf)
 
 --------------------------------------------------------------------------------
@@ -133,82 +111,6 @@ type instance G.ResponseInitialMetadata (Protobuf OMS.MetricsService meth) = G.N
 type instance G.ResponseTrailingMetadata (Protobuf OMS.MetricsService meth) = G.NoMetadata
 
 --------------------------------------------------------------------------------
--- OpenTelemetry Exporter Result for Traces
-
-data ExportTraceResult
-  = ExportTraceResult
-  { exportedSpans :: !Int64
-  , rejectedSpans :: !Int64
-  , maybeSomeException :: Maybe SomeException
-  }
-  deriving (Show)
-
-pattern ExportTraceSuccess :: Int64 -> ExportTraceResult
-pattern ExportTraceSuccess exportedSpans =
-  ExportTraceResult exportedSpans 0 Nothing
-
-pattern ExportTraceError :: Int64 -> Int64 -> SomeException -> ExportTraceResult
-pattern ExportTraceError exportedSpans rejectedSpans someException =
-  ExportTraceResult exportedSpans rejectedSpans (Just someException)
-
-data RejectedSpansError
-  = RejectedSpansError
-  { rejectedSpans :: !Int64
-  , errorMessage :: !Text
-  }
-  deriving (Show)
-
-instance Exception RejectedSpansError where
-  displayException :: RejectedSpansError -> String
-  displayException RejectedSpansError{..} =
-    printf "Error: OpenTelemetry Collector rejectedSpans %d data points with message: %s" rejectedSpans errorMessage
-
---------------------------------------------------------------------------------
--- OpenTelemetry gRPC Exporter for Traces
-
-exportResourceSpans ::
-  G.Connection ->
-  ProcessT IO (Tick OTS.ExportTraceServiceRequest) (Tick ExportTraceResult)
-exportResourceSpans conn =
-  construct $ go False
- where
-  go exportedResourceSpans =
-    await >>= \case
-      Tick -> do
-        unless exportedResourceSpans $
-          yield (Item $ ExportTraceSuccess 0)
-        yield Tick
-        go False
-      Item exportTraceServiceRequest -> do
-        exportTraceResult <- liftIO (sendResourceSpans exportTraceServiceRequest)
-        yield (Item exportTraceResult)
-        go True
-
-  sendResourceSpans :: OTS.ExportTraceServiceRequest -> IO ExportTraceResult
-  sendResourceSpans exportTraceServiceRequest =
-    doGrpc `catch` handleGrpcError
-   where
-    !exportedSpans = countSpansInExportTraceServiceRequest exportTraceServiceRequest
-
-    doGrpc :: IO ExportTraceResult
-    doGrpc = do
-      G.nonStreaming conn (G.rpc @(Protobuf OTS.TraceService "export")) (G.Proto exportTraceServiceRequest) >>= \case
-        G.Proto resp
-          | resp ^. OTS.partialSuccess . OTS.rejectedSpans == 0 ->
-              pure $ ExportTraceSuccess exportedSpans
-          | otherwise -> do
-              let !rejectedSpans = resp ^. OTS.partialSuccess . OTS.rejectedSpans
-              let !rejectedMetricsError = RejectedSpansError{errorMessage = resp ^. OTS.partialSuccess . OTS.errorMessage, ..}
-              pure $ ExportTraceError exportedSpans rejectedSpans (SomeException rejectedMetricsError)
-
-    handleGrpcError :: G.GrpcError -> IO ExportTraceResult
-    handleGrpcError grpcError = pure $ ExportTraceError 0 exportedSpans (SomeException grpcError)
-
-type instance G.RequestMetadata (Protobuf OTS.TraceService meth) = G.NoMetadata
-type instance G.ResponseInitialMetadata (Protobuf OTS.TraceService meth) = G.NoMetadata
-type instance G.ResponseTrailingMetadata (Protobuf OTS.TraceService meth) = G.NoMetadata
-
---------------------------------------------------------------------------------
 -- Internal Helpers
 --------------------------------------------------------------------------------
 
@@ -263,34 +165,3 @@ countDataPointsInMetric metric =
         V.length (exponentialHistogram ^. OM.vec'dataPoints)
       Just (OM.Metric'Summary summary) ->
         V.length (summary ^. OM.vec'dataPoints)
-
-{- |
-Internal helper.
-Count the number of `OT.Span` values in an `OTS.ExportTraceServiceRequest`.
--}
-{-# SPECIALIZE countSpansInExportTraceServiceRequest :: OTS.ExportTraceServiceRequest -> Int64 #-}
-{-# SPECIALIZE countSpansInExportTraceServiceRequest :: OTS.ExportTraceServiceRequest -> Word #-}
-countSpansInExportTraceServiceRequest :: (Integral i) => OTS.ExportTraceServiceRequest -> i
-countSpansInExportTraceServiceRequest exportTraceServiceRequest =
-  getSum $ foldMap (Sum . countSpansInResourceSpans) (exportTraceServiceRequest ^. OTS.vec'resourceSpans)
-
-{- |
-Internal helper.
-Count the number of `OT.Span` values in an `OT.ResourceSpans`.
--}
-{-# SPECIALIZE countSpansInResourceSpans :: OT.ResourceSpans -> Int64 #-}
-{-# SPECIALIZE countSpansInResourceSpans :: OT.ResourceSpans -> Word #-}
-countSpansInResourceSpans :: (Integral i) => OT.ResourceSpans -> i
-countSpansInResourceSpans resourceSpans =
-  getSum $ foldMap (Sum . countSpansInScopeSpans) (resourceSpans ^. OT.vec'scopeSpans)
-
-{- |
-Internal helper.
-Count the number of `OT.Span` values in an `OT.ScopeSpans`.
--}
-{-# SPECIALIZE countSpansInScopeSpans :: OT.ScopeSpans -> Int64 #-}
-{-# SPECIALIZE countSpansInScopeSpans :: OT.ScopeSpans -> Word #-}
-countSpansInScopeSpans :: (Integral i) => OT.ScopeSpans -> i
-countSpansInScopeSpans scopeSpans =
-  fromIntegral $
-    V.length (scopeSpans ^. OT.vec'spans)
