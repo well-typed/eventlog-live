8800c93c6abc19f239d735d0db4b11571d1f4bd7
Matthew Pickering <matthewtpickering@gmail.com>
2025-12-02 14:32:50 +0000

    Display function names and set correct service name for profiles

--- eventlog-live-otelcol/src/GHC/Eventlog/Live/Otelcol.hs
+++ eventlog-live-otelcol/src/GHC/Eventlog/Live/Otelcol.hs
@@ -358,7 +358,7 @@ asResourceTelemetryData resource instrumentationScope =
       (resourceProfile, dictionary) <-
         ifNonEmpty
           profiles
-          (processCallStackData instrumentationScope profiles)
+          (processCallStackData resource instrumentationScope profiles)
       pure $
         ResourceTelemetryData'Profile $
           messageWith
