59534a542198fc5b7f8109988fe1de5e243f186b
Wen Kokke <wenkokke@users.noreply.github.com>
2025-12-09 14:13:16 +0000

    fix(eventlog-live): add more primitive numeric types to MetricTypeSing

--- eventlog-live/src/GHC/Eventlog/Live/Data/Metric.hs
+++ eventlog-live/src/GHC/Eventlog/Live/Data/Metric.hs
@@ -8,14 +8,16 @@ module GHC.Eventlog.Live.Data.Metric (
   Metric (..),
   KnownMetric (..),
   MetricTypeSing (..),
+  withNum,
   MetricInstrumentKind (..),
   AggregationTemporailty (..),
 ) where
 
 import Control.Exception (assert)
+import Data.Int (Int16, Int32, Int64, Int8)
 import Data.Kind (Type)
 import Data.Proxy (Proxy)
-import Data.Word (Word32, Word64)
+import Data.Word (Word16, Word32, Word64, Word8)
 import GHC.Eventlog.Live.Data.Attribute (Attrs)
 import GHC.Eventlog.Live.Data.Group (GroupBy (..))
 import GHC.RTS.Events (Timestamp)
@@ -56,9 +58,36 @@ instance (Semigroup a) => Semigroup (Metric a) where
         }
 
 data MetricTypeSing (a :: Type) where
+  MetricTypeSingFloat :: MetricTypeSing Float
   MetricTypeSingDouble :: MetricTypeSing Double
+  MetricTypeSingWord :: MetricTypeSing Word
+  MetricTypeSingWord8 :: MetricTypeSing Word8
+  MetricTypeSingWord16 :: MetricTypeSing Word16
   MetricTypeSingWord32 :: MetricTypeSing Word32
   MetricTypeSingWord64 :: MetricTypeSing Word64
+  MetricTypeSingInt :: MetricTypeSing Int
+  MetricTypeSingInt8 :: MetricTypeSing Int8
+  MetricTypeSingInt16 :: MetricTypeSing Int16
+  MetricTypeSingInt32 :: MetricTypeSing Int32
+  MetricTypeSingInt64 :: MetricTypeSing Int64
+
+{- |
+Get the `Num` instance for a metric type.
+-}
+withNum :: MetricTypeSing a -> ((Num a) => r) -> r
+withNum = \case
+  MetricTypeSingFloat -> id
+  MetricTypeSingDouble -> id
+  MetricTypeSingWord -> id
+  MetricTypeSingWord8 -> id
+  MetricTypeSingWord16 -> id
+  MetricTypeSingWord32 -> id
+  MetricTypeSingWord64 -> id
+  MetricTypeSingInt -> id
+  MetricTypeSingInt8 -> id
+  MetricTypeSingInt16 -> id
+  MetricTypeSingInt32 -> id
+  MetricTypeSingInt64 -> id
 
 data MetricInstrumentKind
   = Sum {aggregationTemporailty :: !AggregationTemporailty, isMonotonic :: !Bool}
