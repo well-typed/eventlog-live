7b4d694d303484d32ae037dfa1850723b40284f4
Wen Kokke <wenkokke@users.noreply.github.com>
2026-07-17 13:51:01 +0100

    fix(demo-nix): use ipedb-0.2.0.1

--- demo-nix/overlay.nix
+++ demo-nix/overlay.nix
@@ -21,7 +21,7 @@ final: prev: with haskell.lib.compose; {
   hs-opentelemetry-otlp = prev.callHackage "hs-opentelemetry-otlp" "0.2.0.0" { };
   http2 = prev.callHackage "http2" "5.3.9" { };
   http2-tls = prev.callHackage "http2-tls" "0.4.5" { };
-  ipedb = prev.callHackage "ipedb" "0.2.0.0" { };
+  ipedb = prev.callHackage "ipedb" "0.2.0.1" { };
   eventlog-socket = prev.callHackage "eventlog-socket" "0.1.3.0" { };
   eventlog-socket-control = prev.callHackage "eventlog-socket-control" "0.1.1.0" { };
   optparse-applicative = prev.callHackage "optparse-applicative" "0.19.0.0" { };
